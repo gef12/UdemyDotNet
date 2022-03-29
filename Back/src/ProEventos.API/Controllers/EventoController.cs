ad926981e3b78af2b97734529d3ad553ca288d58
gef12 <gerfersoncoelho@gmail.com>
2022-03-29 17:44:12 -0300

    initial commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using ProEventos.API.Models;
+//using ProEventos.Persistence.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]
+            {
+                new Evento ()   {
+                        EventoId = 1,
+                        Tema = "angular ",
+                        Local = "BH",
+                        Lote = "1",
+                        QtdPessoas = 250,
+                        DataEvento = DateTime.Now.AddDays(2).ToString(),
+                        ImagemURL = "foto0.png"
+                },
+                new Evento ()   {
+                        EventoId = 2,
+                        Tema = "angular  4",
+                        Local = "rio",
+                        Lote = "3",
+                        QtdPessoas = 350,
+                        DataEvento = DateTime.Now.AddDays(4).ToString(),
+                        ImagemURL = "foto.png"
+                }
+
+            };
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+
+
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+
+
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "post";
+        }
+    }
+}

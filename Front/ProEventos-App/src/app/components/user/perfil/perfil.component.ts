b6a2e308ee159ad95472cfa5c98cd60bc17490f9
gef12 <gerfersoncoelho@gmail.com>
2022-04-12 14:08:54 -0300

    Tratando forms e erros validators em geral

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,15 +1,85 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.scss']
+  styleUrls: ['./perfil.component.scss'],
 })
 export class PerfilComponent implements OnInit {
+  form: FormGroup | any;
 
-  constructor() { }
+  get f(): any {
+    return this.form.controls;
+  }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.validation();
   }
 
+  public validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'confirmPassword'),
+    };
+    this.form = this.fb.group(
+      {
+        titulo: ['NaoInformado', Validators.required],
+        nome: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20),
+          ],
+        ],
+        sobrenome: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        telefone: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(10),
+            Validators.maxLength(11),
+          ],
+        ],
+        funcao: ['NaoInformado', [Validators.required]],
+        descricao: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20),
+          ],
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      formOptions
+    );
+  }
+
+  public resetForm(): void {
+    this.form.reset();
+  }
 }
--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup | any;
 
-  constructor() { }
+  get f(): any {
+    return this.form.controls;
+  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.validation();
   }
 
+  public validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'confirmPassword'),
+    };
+    this.form = this.fb.group(
+      {
+        nome: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20),
+          ],
+        ],
+        sobrenome: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        nomeUser: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20),
+          ],
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      formOptions
+    );
+  }
+
+  public resetForm(): void {
+    this.form.reset();
+  }
+
+  public validarPassword(): any {
+    const password = this.form.get('password').value;
+    const confirmPassword = this.form.get('confirmPassword').value;
+
+    console.log(password === confirmPassword ? true : false);
+    return password === confirmPassword ? null : undefined;
+  }
 }
